348ece2ad3168151c20e150941bbbbf0ccedc73f
soham Kulkarni <sohamkulkarni730@hotmail.com>
2021-02-26 02:29:19 +0530

    Adding whatsAppAnalysis.java for word count and word_pair_count

--- src/main/java/com/soham/sparkPractice/salesDataAnalysis.java
+++ src/main/java/com/soham/sparkPractice/salesDataAnalysis.java
@@ -3,29 +3,69 @@ package com.soham.sparkPractice;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.RelationalGroupedDataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataType;
+import org.apache.spark.sql.types.DataTypes;
+
+import static org.apache.spark.sql.functions.*;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
-public class salesDataAnalysis
-{
+public class salesDataAnalysis {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Logger.getLogger("org").setLevel(Level.ERROR);
 
         SparkSession spark = SparkSession.builder().appName("Sales data analysis").master("local[*]").getOrCreate();
 
-        Dataset<Row> salesData=spark.read().option("header","true").csv("D:\\DataSets\\sales\\2m Sales Records.csv");
+        Dataset<Row> salesData = spark.read().option("header", "true").csv("D:\\DataSets\\sales\\2m Sales Records.csv");
 
-        String[] columns=   salesData.columns() ;
+        String[] columns = salesData.columns();
+        // 0-Region,1-Country,2-Item Type,3-Sales Channel,4-Order Priority,5-Order Date,6-Order ID,7-Ship Date,8-Units Sold
+        //9-Unit Price,10-Unit Cost,11-Total Revenue,12-Total Cost,13-Total Profit
 
-        Arrays.stream(columns).forEach( v -> {
+        // Describe the data type - Nominal, Ordinal, Discrete, Continuous
+        // Group by operation can be done on Ordinal , nominal data
+        //
+       /* Arrays.stream(columns).forEach( v -> {
                                                 System.out.println(v);
-                                                salesData.select(v).distinct().show();
-                                                System.out.println("\n\n\n");
-                                             });
+                                               *//* salesData.select(v).distinct().show();
+                                                System.out.println("\n\n");*//*
+                                             });*/
 
+        // caching improves performance
+        Dataset<Row> newdata = salesData.select(columns[2], columns[1], columns[13]);
+
+        Dataset<Row> newdata1 = newdata.groupBy(columns[1])
+                                       .agg(first(columns[2]), sum((columns[13])))
+                                       .filter(col("first(Item Type)").equalTo("Household"))
+                                       .orderBy("sum(Total Profit)");
+
+
+        Dataset<Row> newdata2 = newdata.groupBy(columns[1]).count();
+               // .agg( max(col(columns[1]).cast(DataTypes.LongType)));
+
+        Dataset<Row> newdata3 = salesData.groupBy(columns[1]).pivot(columns[2]).count();
+
+        Dataset<Row> newdata4 = salesData.groupBy(columns[1],columns[2])
+                                        .count()
+                                        .agg((count(columns[2])));
+
+
+
+        newdata.show();
+        newdata1.show();
+        newdata2.show();
+        newdata3.show();
+        newdata4.show();
+
+
+
+        /*Scanner in = new Scanner(System.in) ;
+        in.nextLine() ;*/
     }
+
 }
---
+++ src/main/java/com/soham/sparkPractice/whatsAppAnalysis.java
@@ -0,0 +1,161 @@
+package com.soham.sparkPractice;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import org.apache.spark.sql.*;
+import scala.Tuple2;
+import scala.Tuple4;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+
+public class whatsAppAnalysis {
+
+    public static void main(String[] agrs) throws IOException {
+
+        Logger.getLogger("org").setLevel(Level.ERROR);
+
+        Long t = System.nanoTime();
+
+        SparkConf conf = new SparkConf().setAppName("whatsapp data analysis").setMaster("local[*]");
+
+        JavaSparkContext spark_ctx = new JavaSparkContext(conf);
+
+        SparkSession spark_ss = SparkSession.builder().config(conf).getOrCreate();
+
+
+// creating a stopword LOCAL corpus
+        Stream<String> stopwords = null;
+        try {
+            stopwords = Files.lines(Paths.get("in/stopwords_en.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        List<String> stopwords_list = stopwords.flatMap(lines -> Arrays.stream(lines.split("\n"))).collect(Collectors.toList());
+        Set<String> stopwords_set = new HashSet<String>();
+        stopwords_set.addAll(stopwords_list);
+
+
+        JavaRDD<Tuple4> chats = spark_ctx.textFile("in/whatsapp_chats/WhatsApp_Chat_AB_SK.txt")
+                .map(v -> chat_regex_ListString(v))
+                .filter(chat -> chat.size() != 0)
+                .map(string ->
+                        new Tuple4<String, String, String, String>(string.get(0),
+                                string.get(1),
+                                string.get(2),
+                                string.get(3).replaceAll("[^a-zA-Z0-9\\s]","").toLowerCase())
+
+                );
+
+        List<String> word_pair_count = chats.flatMap(v -> IntStream.range(1, v._4().toString().split(" ").length).mapToObj(i -> v._4().toString().split(" ")[i-1].concat(" " + v._4().toString().split(" ")[i])).collect(Collectors.toList()).stream().iterator())
+                .mapToPair(v1 -> new Tuple2<String, Long>(v1, 1L))
+                .reduceByKey((v2, v3) -> v2 + v3)
+                .mapToPair(v4 -> new Tuple2<Long, String>(v4._2(), v4._1()))
+                .filter(w2 -> w2._1()>1L)
+                .sortByKey(false)
+                .map(v5 -> v5._2().concat("," + v5._1().toString()))
+                .collect();
+
+
+        Path out = Paths.get("out/whatsapp_chats/WhatsApp_Chat_word_count2.txt");
+        Path out_csv = Paths.get("out/whatsapp_chats/WhatsApp_Chat.csv");
+
+        List<String> chat_list = null;
+
+       if(!Files.exists(out_csv))
+          { chat_list = chats.map(v -> v.toString()).collect() ;
+
+        Files.write(out_csv, chat_list , Charset.forName("ISO-8859-1"));}
+
+        Files.write(out, word_pair_count , Charset.forName("ISO-8859-1"));
+
+        System.out.println(System.nanoTime() - t);
+
+
+
+    }
+
+    public static String chat_regex_CSVString(String input_msg) {
+        // Date (?<=^)(.*?)(?=\,)
+        Pattern date = Pattern.compile("^[0-9\\/]*");
+        // timestamp (?<=,\s)(.*?)(?=\s\-)
+        Pattern timestamp = Pattern.compile("(?<=,\\s)(.*?)(?=\\s-)");
+        // Actor (?<=\-\s)(.*?)(?=\:)
+        Pattern Actor = Pattern.compile("(?<=\\-\\s)(.*?)(?=\\:)");
+        // Message in text  (?<=\:\s)(.*)
+        Pattern Message = Pattern.compile("(?<=\\:\\s)(.*)");
+
+        Matcher m_timestamp = timestamp.matcher(input_msg);
+        Matcher m_date = date.matcher(input_msg);
+        Matcher m_Actor = Actor.matcher(input_msg);
+        Matcher m_Message = Message.matcher(input_msg);
+
+        StringBuilder result = new StringBuilder("");
+
+        if (m_date.find())
+            result.append(m_date.group(0) + ",");
+        if (m_timestamp.find())
+            result.append(m_timestamp.group(0) + ",");
+        if (m_Actor.find())
+            result.append(m_Actor.group(0) + ",");
+        if (m_Message.find())
+            result.append(m_Message.group(0) + ",");
+
+        return result.toString();
+
+    }
+
+    public static List<String> chat_regex_ListString(String input_msg) {
+        // Date (?<=^)(.*?)(?=\,)
+        Pattern date = Pattern.compile("^[0-9\\/]*");
+        // timestamp (?<=,\s)(.*?)(?=\s\-)
+        Pattern timestamp = Pattern.compile("(?<=,\\s)(.*?)(?=\\s-)");
+        // Actor (?<=\-\s)(.*?)(?=\:)
+        Pattern Actor = Pattern.compile("(?<=\\-\\s)(.*?)(?=\\:)");
+        // Message in text  (?<=\:\s)(.*)
+        Pattern Message = Pattern.compile("(?<=\\:\\s)(.*)");
+
+        Matcher m_timestamp = timestamp.matcher(input_msg);
+        Matcher m_date = date.matcher(input_msg);
+        Matcher m_Actor = Actor.matcher(input_msg);
+        Matcher m_Message = Message.matcher(input_msg);
+
+        List<String> result = new ArrayList<String>();
+
+        if (m_date.find() & m_timestamp.find() & m_Actor.find() & m_Message.find()) {
+            result.add(m_date.group(0));
+            result.add(m_timestamp.group(0));
+            result.add(m_Actor.group(0));
+            result.add(m_Message.group(0));
+        }
+
+        return result;
+
+    }
+
+    public static List<String> sentence_word_list(String input) {
+        String[] words = input.split(" ");
+
+        List<String> result = IntStream.range(1, words.length).mapToObj(i -> words[i-1].concat(" " + words[i])).collect(Collectors.toList());
+        List<String> result2 = IntStream.range(2, words.length).mapToObj(i -> words[i-2].concat(" " + words[i-1] +" "+ words[i])).collect(Collectors.toList());
+        //System.out.println(result.size());
+        result.addAll(result2) ;
+        return result;
+     }
+
+
+}
+
